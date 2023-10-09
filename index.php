2ffc62ca6abc3b3cb03cae39e5a70c704b9ac76e
Tino Ruijs <truijs@ruijssoftware.nl>
2023-10-09 21:56:56 +0200

    Wijziging 2156

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Webpagina</title>
+</head>
+<body>
+
+    <h1>Welkom op deze PHP-webpagina!</h1>
+
+    <?php
+        // PHP-code hier
+        $naam = "GPT-3.5";
+        echo "<p>Hallo, mijn naam is $naam. Dit is een eenvoudige PHP-webpagina.</p>";
+    ?>
+
+</body>
+</html>
