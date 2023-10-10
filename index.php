a3b098e02dcb56e19a8e91c85ae21e2f0bd7a5bd
Docent Tino <t.ruijs@kw1c.nl>
2023-10-10 14:22:27 +0200

    2e regel door docent Tino

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 <body>
 
     <h1>Welkom op deze PHP-webpagina! Dit is in h1.</h1>
-
+    <h2>Regel toegevoegd door docent Tino in h2.</h2>
     <?php
         // PHP-code hier
         $naam = "GPT-3.5";
