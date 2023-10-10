1f9fbe010dacf634521331e28cf36d91147ba001
Tino Ruijs <truijs@ruijssoftware.nl>
2023-10-10 12:22:26 +0200

    Aangepaste tekst op website

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Webpagina</title>
+    <title>PHP Webpagina. Dit is in HTML</title>
 </head>
 <body>
 
-    <h1>Welkom op deze PHP-webpagina!</h1>
+    <h1>Welkom op deze PHP-webpagina! Dit is in h1.</h1>
 
     <?php
         // PHP-code hier
