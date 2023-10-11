a5d1232359dd5132f5c38b39d505a2c026dd669e
Docent Tino <t.ruijs@kw1c.nl>
2023-10-11 15:27:03 +0200

    Punt erachter gezet.

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Webpagina. Dit is in HTML</title>
+    <title>PHP Webpagina. Dit is in HTML. (nu met punt)</title>
 </head>
 <body>
 
